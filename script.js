04b5def9dd0738255d514c9e75281a912d1f2298
Bari077 <skyline.agency@yandex.ru>
2022-08-17 20:54:10 +0300

    commit after merge branch JS

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-const $formProfile = document.forms['profile-form'];
-const $inputTitle = $formProfile.elements['profile__title'];
-const $inputJob = $formProfile.elements['profile__subtitle'];
-const $profileTitle = document.querySelector('.profile__title');
-const $profileSubtitle = document.querySelector('.profile__subtitle');
 
-function formSubmitHandler (evt) {
+const $formProfile = document.forms['profile-form'];
+const $inputName = $formProfile.elements['profile__title'];
+const $inputJob = $formProfile.elements['profile__subtitle'];
+const $profileName = document.querySelector('.profile__title');
+const $profileJob = document.querySelector('.profile__subtitle');
+
+function formProfileSubmit (evt) {
     evt.preventDefault();
-    if ($inputTitle.value.length <=0 && $inputJob.value.length <=0) {
+    if ($inputName.value.length <=0 || $inputJob.value.length <=0) {
         alert('Необходимо веести имя и род деятельности');
     }
     else {
-    $profileTitle.textContent = $inputTitle.value;
-    $profileSubtitle.textContent = $inputJob.value;    
+    $profileName.textContent = $inputName.value;
+    $profileJob.textContent = $inputJob.value;    
     document.querySelector('.popup').classList.remove('popup_opened');
     }
 }
 
-$formProfile.addEventListener('submit', formSubmitHandler);
+$formProfile.addEventListener('submit', formProfileSubmit);
 
 
 const $toggleLike = document.querySelectorAll('.card__like');
@@ -27,15 +28,31 @@ $toggleLike.forEach ($likeIcon => {
 });
 
 
-
-const $cardsContainer = document.querySelector('.card');
+const $cardsContainer = document.querySelector('.cards');
 function addCard (cardImg , cardTitle) {
     const cardTemplate = document.querySelector('#card-template').content;
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__title').textContent = cardTitle;
-    cardElement.querySelector('.card__image').src = cardImg;
-    cardElement.querySelector('.card__like').addEventListener('click', function(evt) {
-        evt.target.classList.toggle('active');
+    const $cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    $cardElement.querySelector('.card__title').textContent = cardTitle;
+    $cardElement.querySelector('.card__image').setAttribute('src', cardImg);
+    $cardElement.querySelector('.card__image').setAttribute('alt', cardTitle);
+    $cardElement.querySelector('.card__like').addEventListener('click', function(evt) {
+        evt.target.classList.toggle('card__like_active');
     });
-    $cardsContainer.append(cardElement);
+    $cardsContainer.prepend($cardElement);
 }
+
+const $formAddCard = document.forms['addCard-form'];
+const $inputTitle = $formAddCard.elements['profile__title'];
+const $inputImgLink = $formAddCard.elements['profile__subtitle'];
+function formAddCardSubmit (evt) {
+    evt.preventDefault();
+    if ($inputTitle.value.length <=0 || $inputImgLink.value.length <=0) {
+        alert('Необходимо веести название и ссылку');
+    }
+    else {
+    addCard($inputImgLink.value, $inputTitle.value);
+    document.getElementById('modal-addCard').classList.remove('popup_opened');
+    }
+}
+
+$formAddCard.addEventListener('submit', formAddCardSubmit);
