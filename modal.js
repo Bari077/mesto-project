d805890e403504ae4b2a68c898a86e8ecce249e4
Bari077 <skyline.agency@yandex.ru>
2022-08-17 16:50:00 +0300

    realased modal.js , added saving function for profile, active for like_icons

---
+++ modal.js
@@ -0,0 +1,14 @@
+const $toggleModal = document.querySelectorAll('#openModal');
+$toggleModal.forEach($openButton => {
+    const $modalPopup = document.getElementById($openButton.dataset.target);
+    $openButton.addEventListener('click', () => {
+        $modalPopup.classList.add('popup_opened');
+    });
+    
+    const $closeModal = $modalPopup.querySelectorAll('.popup__close-button');
+    $closeModal.forEach($closeButton => {
+        $closeButton.addEventListener('click', () => {
+            $modalPopup.classList.remove('popup_opened');
+        });
+    });
+}) ;
--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__close-button');
-function callPopup() {
-    let popupWindow = document.querySelector('.popup');
-    if (popupWindow.classList.contains('popup_opened') === true) {
-        popupWindow.classList.remove('popup_opened');
+const $formProfile = document.forms['profile-form'];
+const $inputTitle = $formProfile.elements['profile__title'];
+const $inputJob = $formProfile.elements['profile__subtitle'];
+const $profileTitle = document.querySelector('.profile__title');
+const $profileSubtitle = document.querySelector('.profile__subtitle');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    if ($inputTitle.value.length <=0 && $inputJob.value.length <=0) {
+        alert('Необходимо веести имя и род деятельности');
     }
     else {
-        popupWindow.classList.add('popup_opened');
+    $profileTitle.textContent = $inputTitle.value;
+    $profileSubtitle.textContent = $inputJob.value;    
+    document.querySelector('.popup').classList.remove('popup_opened');
     }
-
 }
-editButton.addEventListener('click', callPopup);
-closeButton.addEventListener('click', callPopup);
+
+$formProfile.addEventListener('submit', formSubmitHandler);
+
+
+const $toggleLike = document.querySelectorAll('.card__like');
+$toggleLike.forEach ($likeIcon => {
+    $likeIcon.addEventListener('click', () => {
+        $likeIcon.classList.toggle($likeIcon.dataset.target);
+    });
+});
+
+
+
+const $cardsContainer = document.querySelector('.card');
+function addCard (cardImg , cardTitle) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = cardTitle;
+    cardElement.querySelector('.card__image').src = cardImg;
+    cardElement.querySelector('.card__like').addEventListener('click', function(evt) {
+        evt.target.classList.toggle('active');
+    });
+    $cardsContainer.append(cardElement);
+}
